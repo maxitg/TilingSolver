d82d9fc0bdcde57023f72200f9297281ce41aa77
Max Piskunov <maxitg@icloud.com>
2021-08-25 00:55:06 -0500

    Make git push less likely to fail.

--- tileAndShutDown.sh
+++ tileAndShutDown.sh
@@ -8,13 +8,15 @@ size1=$2
 size2=$3
 maskID=$4
 
+git pull
 echo "Searching minimal sets for "$size1"-"$size2"-"$maskID"..."
 wolframscript -entitlement "$1" -code "<<MinimalSetsSearcher.wl; FindMinimalSets[{"$size1", "$size2"}, "$maskID"];"
 
 echo "Done. Committing the results..."
+git checkout -b "auto-"$size1"-"$size2"-"$maskID""
 git add .
 git commit -m "Complete results for "$size1"-"$size2"-"$maskID"."
-git push
+git push -u origin "auto-"$size1"-"$size2"-"$maskID""
 
 echo "Shutting down..."
 sudo shutdown
