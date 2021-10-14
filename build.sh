2f182b6d3fc53d60a1d68ca68a17dfcff875b218
Max Piskunov <maxitg@icloud.com>
2021-10-14 14:24:29 -0500

    Add pipefail.

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 tilerRoot=$(cd "$(dirname "$0")" && pwd)
 cd "$tilerRoot"
 
 echo "Building minimal-tiling-set..."
 mkdir -p bin
-g++ -O3 -std=c++17 -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include -lcryptominisat5 -lboost_filesystem -pthread -lssl -lcrypto minimal-tiling-sets/minimal-tiling-sets/*.cpp -o bin/minimal-tiling-sets
+g++ -O3 -std=c++17 -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include \
+                   -lcryptominisat5 -lboost_filesystem -pthread -lssl -lcrypto \
+                   minimal-tiling-sets/minimal-tiling-sets/*.cpp -o bin/minimal-tiling-sets
 echo "Build done."
