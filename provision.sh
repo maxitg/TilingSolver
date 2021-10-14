f2d40805d95eccace7b30f16fbbf31b535072798
Max Piskunov <maxitg@icloud.com>
2021-10-14 14:10:34 -0500

    Update provision script for Ubuntu.

--- provision.sh
+++ provision.sh
@@ -6,36 +6,13 @@ echo "Found "$NUMCPUS" CPUs"
 
 cd ~/git;
 
-# Install dependencies from yum
-echo "Installing dependencies from yum..."
-sudo yum -y install cmake3 zlib-devel sqlite-devel help2man autoconf automake libtool mpi-devel gcc-c++ openssl11-devel
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/usr/lib64/openmpi/lib' >> ~/.bash_profile
-echo 'export PATH="$PATH":/usr/lib64/openmpi/bin/' >> ~/.bash_profile
-source ~/.bash_profile
-echo "Done installing dependencies from yum."
-
-echo "Installing boost..."
-git clone --recursive https://github.com/boostorg/boost.git
-cd boost
-./bootstrap.sh
-./b2 -j $NUMCPUS
-sudo ./b2 install
-source ~/.bash_profile
-cd ..
-echo "Done installing boost."
-
-# Install m4ri
-echo "Installing m4ri..."
-git clone https://bitbucket.org/malb/m4ri.git
-cd m4ri
-autoreconf --install
-./configure --enable-openmp
-make -j$NUMCPUS
-sudo make install
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/usr/local/lib' >> ~/.bash_profile
-source ~/.bash_profile
-cd ..
-echo "Done installing m4ri."
+echo "Installing dependencies from apt-get..."
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential cmake zlib1g-dev libm4ri-dev libsqlite3-dev help2man libboost-dev \
+                        libboost-program-options-dev gcc libssl-dev
+source ~/.bashrc
+echo "Done installing dependencies from apt-get."
 
 # Install cryptominisat
 echo "Installing cryptominisat..."
@@ -43,12 +20,9 @@ git clone https://github.com/msoos/cryptominisat.git
 cd cryptominisat
 mkdir build
 cd build
-cmake3 ..
+cmake ..
 make -j$NUMCPUS
 sudo make install
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/usr/local/lib64' >> ~/.bash_profile
-source ~/.bash_profile
-sudo ldconfig
 cd ../..
 echo "Done installing cryptominisat."
 
@@ -58,7 +32,7 @@ git clone https://github.com/nlohmann/json.git
 cd json
 mkdir build
 cd build
-cmake3 ..
+cmake ..
 make -j$NUMCPUS
 sudo make install
 cd ../..
