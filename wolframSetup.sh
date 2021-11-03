2fd57725b706595fc2ad60b4caacc93f1efd3f93
Max Piskunov <maxitg@icloud.com>
2021-11-03 18:44:42 -0500

    Remove Wolfram setup.

--- wolframSetup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-echo "Setting DropboxDataDirectory as /Physics/Data/Tilings/minimal-sets-v3 in ~/.minimal-tiling-sets..."
-echo '{"DropboxDataDirectory": "/Physics/Data/Tilings/minimal-sets-v3"}' > ~/.minimal-tiling-sets
-echo "Done."
